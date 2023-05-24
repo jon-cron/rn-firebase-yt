c68227e62d5a000d6f8b7c07d0d3efbab1934093
jon-cron <jonathanacm95@gmail.com>
2023-05-24 09:13:42 -0600

    push

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,40 +9,23 @@ import {
 } from "react-native";
 import { signOut } from "firebase/auth";
 import { auth, db } from "../firebase/firebase_config";
-// import { useDocument } from "../hooks/useDocument";
+import { useDocument } from "../hooks/useDocument";
 const HomeScreen = ({ navigation }) => {
   let testId = "34Z6PS9QTWK5rqmCpm5I";
-  const [document, setDocument] = useState(null);
-  const [isPending, setIsPending] = useState(null);
-  // const { document, isPending } = useDocument("test", testId);
-  // const [dbDocument, setDbDocument] = useState();
+  const { document } = useDocument("test", testId);
   const handleLogout = async () => {
     await signOut(auth);
     navigation.replace("Login");
   };
-  useEffect(() => {
-    setIsPending(true);
-    // NOTE here is a good example of how to get a snapshot of a single doc using Firebase 9
-    const unsub = async () => {
-      const jobRef = doc(db, "test", testId).then(
-        onSnapshot(jobRef, (snapshot) => {
-          setDocument(snapshot.data());
-        })
-      );
-      setIsPending(false);
-    };
-    return () => unsub();
-  }, []);
-  if (isPending != null) {
-    return (
-      <View style={styles.container}>
-        <Text>{document?.text}</Text>
-        <TouchableOpacity onPress={handleLogout}>
-          <Text>Logout</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{document?.text}</Text>
+      <TouchableOpacity onPress={handleLogout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
