56ad85ccb26941195e17c04f669794d6e3a5bf03
jon-cron <jonathanacm95@gmail.com>
2023-05-24 08:46:22 -0600

    still not fetching data before page load

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,23 +8,35 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { signOut } from "firebase/auth";
-import { auth } from "../firebase/firebase_config";
-import { useDocument } from "../hooks/useDocument";
+import { auth, db } from "../firebase/firebase_config";
+// import { useDocument } from "../hooks/useDocument";
 const HomeScreen = ({ navigation }) => {
   let testId = "34Z6PS9QTWK5rqmCpm5I";
-  const { document, isPending } = useDocument("test", testId);
-  const [dbDocument, setDbDocument] = useState(null);
+  const [document, setDocument] = useState(null);
+  const [isPending, setIsPending] = useState(null);
+  // const { document, isPending } = useDocument("test", testId);
+  // const [dbDocument, setDbDocument] = useState();
   const handleLogout = async () => {
     await signOut(auth);
     navigation.replace("Login");
   };
-  useLayoutEffect(() => {
-    setDbDocument(document);
-  }, [document, isPending]);
-  if (document && isPending === false) {
+  useEffect(() => {
+    setIsPending(true);
+    // NOTE here is a good example of how to get a snapshot of a single doc using Firebase 9
+    const unsub = async () => {
+      const jobRef = doc(db, "test", testId).then(
+        onSnapshot(jobRef, (snapshot) => {
+          setDocument(snapshot.data());
+        })
+      );
+      setIsPending(false);
+    };
+    return () => unsub();
+  }, []);
+  if (isPending != null) {
     return (
       <View style={styles.container}>
-        <Text>{dbDocument?.text}</Text>
+        <Text>{document?.text}</Text>
         <TouchableOpacity onPress={handleLogout}>
           <Text>Logout</Text>
         </TouchableOpacity>
